77229fbdece826cceb1e325d8c1370dd4bf367fc
Kai Wei <43118998+Kai-Wei-626@users.noreply.github.com>
2018-10-31 00:00:03 -0700

    Update two sum.py

--- day1/two sum.py
+++ day1/two sum.py
@@ -1,1 +1,12 @@
+## 2 sum
 
+nums = [2, 7, 11, 15]
+target = 9
+def twosum(nums, target):
+    dic = {}
+    for index, num in enumerate(nums):
+        if num in dic:
+            return [dic[num],index]
+        dic[target - num] = index
+ 
+# other than this, we can use brute force
