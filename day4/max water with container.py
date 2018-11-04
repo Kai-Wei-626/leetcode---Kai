4d5d6db9ca23b4e958d953a950894719b14f4dfa
Kai Wei <43118998+Kai-Wei-626@users.noreply.github.com>
2018-11-04 09:47:29 -0700

    Update max water with container.py

--- day4/max water with container.py
+++ day4/max water with container.py
@@ -1,3 +1,4 @@
+# brute force
 class Solution:
     def maxArea(self, height):
         """
@@ -14,4 +15,29 @@ class Solution:
         return max_area
         
         
+        
+#solution 2, 2 pointer
+'''
+the whole idea of this algo is that by comparing the values of left pointer and right pointer, while one of them is lower 
+or equal to the other, that one moves. it is something like you have to give the chance to that pointer to look for next taller
+bar, the distance between two pointers is always decreasing, so don't need to worry about it.
+'''
+
+
+class Solution:
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left = 0
+        right = len(height)-1
+        maxArea = 0
+        while left < right:
+            maxArea = max(maxArea, min(height[left], height[right]) * (right - left))
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return maxArea
         
