633f6b94d4008d224efe2c8daac3575b6a6aedfb
Kai Wei <43118998+Kai-Wei-626@users.noreply.github.com>
2018-11-16 11:31:23 -0700

    Create 088.Merged 2 Sorted List.py

---
+++ 088.Merged 2 Sorted List.py
@@ -0,0 +1,27 @@
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        # i is the pointer to nums1
+        # j is the pointer to nums2
+        for x in reversed(range(len(nums1))):
+            if m <= 0:
+                for k in range(n):
+                    nums1[k] = nums2[k]
+                break
+            if n <= 0:
+                break
+
+            if nums1[m-1] > nums2[n-1]:
+                nums1[x] = nums1[m-1]
+                m = m - 1
+            else:
+                nums1[x] = nums2[n-1]
+                n = n - 1
+
+            
