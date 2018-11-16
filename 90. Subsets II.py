ac171aa747762ab155bfbfe6fcc6af4b034b67e8
Kai Wei <43118998+Kai-Wei-626@users.noreply.github.com>
2018-11-16 13:59:03 -0700

    Update 90. Subsets II.py

--- 90. Subsets II.py
+++ 90. Subsets II.py
@@ -24,3 +24,37 @@ class Solution:
             self.helper(nums, i+1, k, cur, res)
             cur.pop()
             
+
+#solution 2
+'''
+s is the start index, which helps the algo dedup during the DFS
+'''
+
+class Solution:
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums = sorted(nums)
+        res = []
+        cur = []
+        s = 0
+        
+        self.helper(nums, s, cur, res)
+        return res
+    
+    def helper(self, nums, s, cur, res): # s is the index of start index
+  
+        res.append(list(cur))
+
+        for i in range(s, len(nums)):
+            if i != s and nums[i] == nums[i-1]: # if contion i!=s must be the first condition as we have to avoid nums[0] == nums[-1] situation
+                continue
+            cur.append(nums[i])
+            self.helper(nums, i+1, cur, res)
+            cur.pop()
+            
+            
+            
+            
