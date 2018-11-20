8cc6fffd94a2ddf0e8a8763e9eb0c819818ea7c5
Kai Wei <43118998+Kai-Wei-626@users.noreply.github.com>
2018-11-19 22:22:15 -0700

    Create 102. Binary Tree Level Order Traversal.py

---
+++ 102. Binary Tree Level Order Traversal.py
@@ -0,0 +1,48 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        h = self.height(root) 
+        res = []
+        
+        for i in range(1, h+1):
+            cur = []
+            self.printNode(root, i, cur)
+            res.append(cur)
+            
+        return res
+    
+    #https://www.geeksforgeeks.org/write-a-c-program-to-find-the-maximum-depth-or-height-of-a-tree/    
+    #this is really helpful to understand depth funciton
+    def height(self, root):
+        if root is None:
+            return 0
+        else:
+            lheight = self.height(root.left)
+            rheight = self.height(root.right)
+            
+            if lheight > rheight:
+                return lheight + 1
+            else:
+                return rheight + 1
+            
+    def printNode(self, root, level, cur):
+        if root is None:
+            return
+        if level == 1:
+            cur.append(root.val)
+        if level > 1:
+            self.printNode(root.left, level - 1, cur)
+            self.printNode(root.right, level - 1, cur)
+        
+        
+        
