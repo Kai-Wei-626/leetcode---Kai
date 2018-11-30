f555e86e9783cc8d6e026a4cfb8db6f923178e91
Kai Wei <43118998+Kai-Wei-626@users.noreply.github.com>
2018-11-30 15:13:58 -0700

    Update 127. Word Ladder.py

--- 127. Word Ladder.py
+++ 127. Word Ladder.py
@@ -33,3 +33,38 @@ class Solution(object):
             cur_level = next_level
             next_level = []
         return 0
+
+'''
+Bidirectional BFS
+switch s1, s2 when len(s1) > len(s2) so that only expand the shorter list
+
+firstly, look at if new word is in s2, if failed, then
+secondly, look at if new word is in wordList
+
+s1 = s, this operation is same as it in one direction solution.
+'''
+class Solution(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+        wordDict = set(wordList)
+        if endWord not in wordDict: return 0
+        
+        l = len(beginWord)
+        s1 = [beginWord]
+        s2 = [endWord]
+        wordDict.remove(endWord)
+        step = 0
+        while len(s1) > 0 and len(s2) > 0:
+            step += 1
+            if len(s1) > len(s2): s1, s2 = s2, s1
+            s = []  
+            for w in s1:
+                new_words = [
+                    w[:i] + t + w[i+1:]  for t in 'abcdefghijklmnopqrstuvwxyz' for i in range(l)]
+                for new_word in new_words:
+                    if new_word in s2: return step + 1
+                    if new_word in wordDict:
+                        wordDict.remove(new_word)                        
+                        s.add(new_word)
+            s1 = s
+        
+        return 0    
