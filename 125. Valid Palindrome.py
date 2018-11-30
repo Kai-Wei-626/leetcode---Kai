f4bdef62fe10c3c91b774a8d3bd2879c75a3cd70
Kai Wei <43118998+Kai-Wei-626@users.noreply.github.com>
2018-11-30 11:37:23 -0700

    Create 125. Valid Palindrome.py

---
+++ 125. Valid Palindrome.py
@@ -0,0 +1,30 @@
+'''
+1. 2 pointer
+2. if not alphabetic character, jump over it. 
+'''
+
+class Solution:
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            
+            while l < r and not s[r].isalnum():
+                r -= 1
+            
+            if s[l].lower() != s[r].lower():
+                return False
+        
+            l += 1
+            r -= 1
+        
+        return True
+            
+        
+        
