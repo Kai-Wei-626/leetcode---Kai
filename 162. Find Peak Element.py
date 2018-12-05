087e183b44e9ff39766a02ce554aae2cd13497c1
Kai Wei <43118998+Kai-Wei-626@users.noreply.github.com>
2018-12-04 22:14:07 -0700

    Update 162. Find Peak Element.py

--- 162. Find Peak Element.py
+++ 162. Find Peak Element.py
@@ -34,4 +34,33 @@ class Solution(object):
                 
         
         
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        # compare the mid element with last elemnet
+        # if mid > last, go to left
+        # if mid <= last, go to right
+        if len(nums) == 0:
+            return None
+
+        
+        left = 0
+        right = len(nums) - 1
+        
+        while left < right:
+            mid = (left + right)//2
+            
+            if nums[mid] > nums[mid+1]:
+                right = mid
+            else:
+                left = mid + 1
+            
+        return left
+                
+        
+        
         
