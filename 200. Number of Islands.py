58768899979956b5d08843ecd3d6f33b9504c064
Kai Wei <43118998+Kai-Wei-626@users.noreply.github.com>
2018-12-10 20:59:24 -0700

    Create 200. Number of Islands.py

---
+++ 200. Number of Islands.py
@@ -0,0 +1,33 @@
+'''
+This solution is straightforward, very easy to understand
+'''
+
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid:
+            return 0
+        
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.helper(grid, i,j)
+                    count += 1
+        
+        return count
+                    
+        
+    def helper(self, grid, i, j):
+        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
+            return
+        grid[i][j] = '#'
+        self.helper(grid, i-1, j)
+        self.helper(grid, i+1, j)
+        self.helper(grid, i, j+1)
+        self.helper(grid, i, j-1)
+        
+        
